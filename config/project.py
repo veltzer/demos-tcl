979cc40cf45265f6e11ab6694993f1c4e7c8e72f
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:50:58 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "demos-tcl"
-project_long_description = "Demos for the tcl language"
-project_year_started = 2015
-project_description = """Demos for the tcl language"""
+description_short = "Demos for the tcl language"
