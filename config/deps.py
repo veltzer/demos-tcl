fc99ad436cfd813bcaa8b561bc73ae3a9ca96713
Mark Veltzer <mark.veltzer@gmail.com>
2022-10-14 19:00:42 +0300

    deps change

--- config/deps.py
+++ config/deps.py
@@ -1,8 +1,8 @@
 packages=[
-    # "tcl{tcl_version}",
-    # "tcl{tcl_version}-dev",
-    # "tcl{tcl_version}-dbgsym",
-    # "tcl{tcl_version}-doc"
+    # f"tcl{tcl_version}",
+    # f"tcl{tcl_version}-dev",
+    # f"tcl{tcl_version}-dbgsym",
+    # f"tcl{tcl_version}-doc"
     "tk",
     "tcl",
     "tcl-doc",
